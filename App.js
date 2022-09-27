53a29249dccb4ebf7228b0a24c90613d664f573d
putusel <elina.putus@gmail.com>
2022-09-27 19:00:28 +0300

    mapview, marker, location

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from'react-native-maps';
+import * as Location from'expo-location';
 
 export default function App() {
   return (
